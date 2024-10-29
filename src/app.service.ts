db607890a257a69417d314891bf47ec0a9ecb253
Ale Duarte <duartemattias4@gmail.com>
2024-10-28 22:18:39 -0400

    agregando conexion a base de datos

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  getProducts(): string[] {
+    return [
+      "Producto 1",
+      "Producto 2",
+    ]
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hola Muundo';
   }
 }
